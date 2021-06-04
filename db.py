a771774dc2f76995f9de418a4153686c2fa73ef2
cs73 <hjrand1005@gmail.com>
2021-06-04 15:13:56 -0400

    FIX: db.py adds admin, requirements.txt updated.

--- db.py
+++ db.py
@@ -11,10 +11,10 @@ if __name__ == '__main__':
     db.create_all()
     # check if admin user exists in db, for debugging
     # creates admin user in db with username, password: 'admin', 'admin' for testing purposes
-    # admin = User.query.filter_by(username='admin').first()
-    # if not admin:
-    #    hashed = generate_password_hash('admin', method='sha256')
-    #    admin_user = User(public_id=str(
-    #        uuid.uuid1()), username='admin', password=hashed, longest_streak=0, admin=True)
-    #    db.session.add(admin_user)
-    #    db.session.commit()
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
+        hashed = generate_password_hash('admin', method='sha256')
+        admin_user = User(public_id=str(
+            uuid.uuid1()), username='admin', password=hashed, longest_streak=0, admin=True)
+        db.session.add(admin_user)
+        db.session.commit()
